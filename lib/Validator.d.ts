a7185c8752ecc11e2e84bf03ffc7fedcc9ae85ef
Tareq Hossain <xtrinsic96@gmail.com>
2021-11-06 20:15:06 +0600

    first build

---
+++ lib/Validator.d.ts
@@ -0,0 +1,10 @@
+export declare class Validator {
+    _fields: any;
+    _values: any;
+    errors: any;
+    constructor(fields: any, values: any);
+    setFields(fields: any): void;
+    setValues(values: any): void;
+    validate(): any;
+}
+//# sourceMappingURL=Validator.d.ts.map
---
+++ lib/index.d.ts
@@ -0,0 +1,2 @@
+export declare const validate: (rules: any, fields: any) => any;
+//# sourceMappingURL=index.d.ts.map
