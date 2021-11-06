1937483843a32fb18a24ebdf09293254275f28fe
Tareq Hossain <xtrinsic96@gmail.com>
2021-11-06 20:13:23 +0600

    added validator

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-export const validate = ()=>{
-    console.log("validating");
+import { Validator } from "./Validator";
+
+export const validate = (rules:any,fields:any)=>{
+    let errors:any = []
+    const validator = new Validator(rules,fields)
+    errors = validator.validate();
+    // console.log(errors);
+
+    return errors
 }
