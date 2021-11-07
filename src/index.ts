15fed5fd2db3d5e5c642035733884a17cbffa875
Tareq Hossain <xtrinsic96@gmail.com>
2021-11-07 07:43:57 +0600

    clean

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,9 @@
 import { Validator } from "./Validator";
 
 export const Validate =(rules:any,fields:any)=>{
-    console.log(rules,fields)
     let errors:any = []
     const validator = new Validator(rules,fields)
     errors = validator.validate();
-    console.log(errors);
-
     return errors
 }
 
